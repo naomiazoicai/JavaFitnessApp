2e437523c1c4f8276555bc8c0e4d46e47f872b0f
naomiazoicai <naomi.azoicai@stud.ubbcluj.ro>
2023-10-23 13:02:50 +0300

    naomi plays with commits

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
 //    Projekt: Java-Konsolenanwendung
 //    Dieses Dokument dient als Leitfaden für die Entwicklung einer Konsolenanwendung in Java.
+    public static void main(String[] args)
+    {
+
+    }
 }
