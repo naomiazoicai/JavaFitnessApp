174a7e52a51780577e7568365e22a1d7a56d5cb4
naomiazoicai <naomi.azoicai@stud.ubbcluj.ro>
2023-10-26 10:51:41 +0300

    implemented ui, in progress

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import UI.UI;
+
 public class Main {
 //    Projekt: Java-Konsolenanwendung
 //    Dieses Dokument dient als Leitfaden für die Entwicklung einer Konsolenanwendung in Java.
     public static void main(String[] args)
     {
+         UI ui = new UI();
+         ui.runUi();
 
     }
 }
--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,4 +1,117 @@
-package ui;
+package UI;
+
+import domain.gym.*;
+import domain.money.Budget;
+import domain.persons.Customer;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UI implements IUi, ISubject{
-}
+    public void showAllEquipmentItems(){
+//        ArrayList<EquipmentItem> equipmentItems = repository.getAllEquipmentItems();
+//        for(EquipmentItem equipmentItem : equipmentItems)
+//        {
+//            System.out.println(equipmentItem);
+//        }
+    }
+    public void showAllExercises(){
+//        ArrayList<Exercise> exercises = repository.getAllExercises();
+//        for(Exercise exercise : exercises)
+//        {
+//            System.out.println(exercise);
+//        }
+    }
+    public void showAllSpecialisedRooms(){
+//        ArrayList<SpecialisedRoom> specialisedRooms = repository.getAllSpecialisedRooms();
+//        for(SpecialisedRoom specialisedRoom : specialisedRooms)
+//        {
+//            System.out.println(specialisedRooms);
+//        }
+    }
+
+    public void showAllWorkouts(){
+//        ArrayList<Workout> workouts = repository.getAllWorkouts();
+//        for(Workout workout : workouts)
+//        {
+//            System.out.println(workouts);
+//        }
+    }
+
+    public void showBudget(){
+//        Budget budget = repository.getBudget();
+   }
+    public void showAllCustomers() {
+//        ArrayList<Customer> customers = repository.getAllCustomers();
+//        for(Customer customer : customers)
+//        {
+//            System.out.println(customer);
+//        }
+    }
+        public void showAllTrainers() {
+//        ArrayList<Trainer> trainers = repository.getAllTrainers();
+//        for(Trainer trainer : trainers)
+//        {
+//            System.out.println(trainers);
+ //       }
+        }
+
+        public void runUi(){
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println(
+                        "1. See all equipment items\n" +
+                        "2. See all exercises\n" +
+                        "3. See all rooms\n" +
+                        "4. See all workouts\n" +
+                        "5. See the total budget\n" +
+                        "6. See all customers\n" +
+                        "7. See all trainers\n" +
+                                "Please enter your choice (1-7):");
+                int input = scanner.nextInt();
+
+            switch(input) {
+                case 1:
+                    System.out.println("...");
+                    //method call equipment item
+                    runUi();
+                    break;
+                case 2:
+                    System.out.println("...");
+                    //method call exercise
+                    runUi();
+                    break;
+                case 3:
+                    System.out.println("...");
+                    //method call rooms
+                    runUi();
+                    break;
+                case 4:
+                    System.out.println("...");
+                    //method call workout
+                    runUi();
+                    break;
+                case 5:
+                    System.out.println("...");
+                    //method call budget
+                    runUi();
+                    break;
+                case 6:
+                    System.out.println("...");
+                    //method call customer
+                    runUi();
+                    break;
+                case 7:
+                    System.out.println("...");
+                    //method call trainer
+                    runUi();
+                    break;
+                default:
+                    System.out.println("bravo,Naomiiiiiiiiiii");
+            }
+            scanner.close();
+            }
+        }
+
+
