c07cb3ae2d788d57cec8900f72f4c8e07130c79c
thecosminel <thecosminel@gmail.com>
2023-10-26 10:52:50 +0300

    implemented InMemoryRepository.java

--- src/domain/persons/Customer.java
+++ src/domain/persons/Customer.java
@@ -2,10 +2,12 @@ package domain.persons;
 
 import domain.money.SubscriptionType;
 
+import java.time.LocalDate;
+
 public class Customer extends Person{
 
     private SubscriptionType subscriptionType;
-    public Customer(String username, String name, String birthDate, Gender gender) {
+    public Customer(String username, String name, LocalDate birthDate, Gender gender) {
         super(username, name, birthDate, gender);
     }
 
