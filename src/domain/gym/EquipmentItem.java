767d68ced541da8a4475c0968b7058951721d5c7
naomiazoicai <naomi.azoicai@stud.ubbcluj.ro>
2023-10-26 09:11:32 +0300

    implemented the gym entities

--- src/domain/gym/EquipmentItem.java
+++ src/domain/gym/EquipmentItem.java
@@ -1,4 +1,51 @@
 package domain.gym;
 
+import java.util.Objects;
+
 public class EquipmentItem {
+    private int ID;
+    private String name;
+
+
+    public EquipmentItem(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+
+    }
+
+    @Override
+    public String toString() {
+        return "EquipmentItem{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EquipmentItem that = (EquipmentItem) o;
+        return ID == that.ID;
+    }
+
+
 }
--- src/domain/gym/Room.java
+++ src/domain/gym/Room.java
@@ -1,4 +1,56 @@
 package domain.gym;
 
+import java.util.Objects;
+
 public class Room {
+    private int id;
+    private boolean cleaned;
+    private boolean occupied;
+
+    public Room(int id, boolean cleaned, boolean occupied) {
+        this.id = id;
+        this.cleaned = cleaned;
+        this.occupied = occupied;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", cleaned=" + cleaned +
+                ", occupied=" + occupied +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return id == room.id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCleaned() {
+        return cleaned;
+    }
+
+    public void setCleaned(boolean cleaned) {
+        this.cleaned = cleaned;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
 }
