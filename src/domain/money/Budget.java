1dd0b03cd7169178e55b3b27127d1593d11d4a20
thecosminel <thecosminel@gmail.com>
2023-10-26 09:57:37 +0300

    updated Budget.java declaration of InMemoryRepository.java

--- src/domain/money/Budget.java
+++ src/domain/money/Budget.java
@@ -11,9 +11,20 @@ public class Budget {
         if (instance == null)
         {
             instance = new Budget();
+            instance.currentMoney = 0;
         }
         return instance;
     };
+
+    public void addMoney(int value)
+    {
+        currentMoney += value;
+    }
+
+    public void spendMoney(int value) throws Exception {
+        if (value > currentMoney) throw new Exception("Expense exceeds budget");
+        currentMoney -= value;
+    }
 
     public double getCurrentMoney() {
         return currentMoney;
