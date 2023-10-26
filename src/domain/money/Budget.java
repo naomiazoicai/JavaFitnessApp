79e7e1a29d0814ea21fb82ce77a0157a579cfb45
naomiazoicai <naomi.azoicai@stud.ubbcluj.ro>
2023-10-26 09:40:55 +0300

    implemented the budget entities

--- src/domain/money/Budget.java
+++ src/domain/money/Budget.java
@@ -2,4 +2,33 @@ package domain.money;
 
 // Singleton
 public class Budget {
+    private static Budget instance;
+    private double currentMoney;
+
+    private Budget(){}
+    public static Budget getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Budget();
+        }
+        return instance;
+    };
+
+    public double getCurrentMoney() {
+        return currentMoney;
+    }
+
+    public void setCurrentMoney(double currentMoney) {
+        this.currentMoney = currentMoney;
+    }
+
+    @Override
+    public String toString() {
+        return "Budget{" +
+                "currentMoney=" + currentMoney +
+                '}';
+    }
+
+
 }
--- src/domain/money/CustomerSubscription.java
+++ src/domain/money/CustomerSubscription.java
@@ -1,4 +1,62 @@
 package domain.money;
 
-public class CustomerSubscription {
+import domain.persons.Customer;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class CustomerSubscription extends SubscriptionType{
+    private Customer customer;
+    private LocalDate validFrom;
+    private LocalDate validUntil;
+
+    public CustomerSubscription(String name, String description, double price, Customer customer, LocalDate validFrom, LocalDate validUntil) {
+        super(name, description, price);
+        this.customer = customer;
+        this.validFrom = validFrom;
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerSubscription{" +
+                "customer=" + customer +
+                ", validFrom=" + validFrom +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDate getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(LocalDate validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public LocalDate getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDate validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CustomerSubscription that = (CustomerSubscription) o;
+        return Objects.equals(customer, that.customer) && Objects.equals(validFrom, that.validFrom) && Objects.equals(validUntil, that.validUntil);
+    }
+
 }
