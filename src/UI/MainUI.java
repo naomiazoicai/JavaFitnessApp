fd30363634237894ab96f39f50da8c9bb7f2ce9f
cosmi <thecosminel@gmail.com>
2023-11-13 00:16:39 +0200

    run() method implemented for each UI MainUI.java to be implemented..

--- src/UI/MainUI.java
+++ src/UI/MainUI.java
@@ -1,9 +1,30 @@
 package UI;
 
+import UI.SpecialisedUIs.*;
+
 public class MainUI {
+    private final BudgetUI budgetUI;
+    private final CustomerUI customerUI;
+    private final TrainerUI trainerUI;
+    private final SubscriptionTypeUI subscriptionTypeUI;
+    private final CustomerSubscriptionUI customerSubscriptionUI;
+    private final SpecialisedRoomUI specialisedRoomUI;
+    private final EquipmentItemUI equipmentItemUI;
+    private final ExerciseUI exerciseUI;
+
     private static MainUI instance;
 
-    private MainUI(){}
+    private MainUI()
+    {
+        budgetUI = BudgetUI.getInstance();
+        customerUI = CustomerUI.getInstance();
+        trainerUI = TrainerUI.getInstance();
+        subscriptionTypeUI = SubscriptionTypeUI.getInstance();
+        customerSubscriptionUI = CustomerSubscriptionUI.getInstance();
+        specialisedRoomUI = SpecialisedRoomUI.getInstance();
+        equipmentItemUI = EquipmentItemUI.getInstance();
+        exerciseUI = ExerciseUI.getInstance();
+    }
 
     public static MainUI getInstance()
     {
